0ea7b29a8207d951015d647b2ea5d5589c555611
Rui Cao <rayray199085@gmail.com>
2019-01-23 15:07:53 +1100

    Initial Commit

---
+++ ScissorsHammer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ScissorsHammer
+//
+//  Created by Stephen Cao on 23/1/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
